886125297879f4c6f8d6c19ec1074f71858ca68a
Sergio <sagg_301@outlook.com>
2020-03-05 17:52:19 -0400

    Cambios de color e iconos

--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -9,21 +9,21 @@ class Receipt extends React.Component {
             <Card style={{ width: '450px' }}>
                 <Card.Img variant="top" src={check_mark} width="200" height="200" />
                 <Card.Body>
-                    <Card.Title> <h3> Pago realizado con éxito </h3></Card.Title>
+                    <Card.Title> <h3 style={{ color: "#5D9BEB" }}> Pago realizado con éxito </h3></Card.Title>
                     <Card.Text>
                         <Row className="justify-content-md-center">
                             <Col ls={12}>
-                                <h4>Total: Bs {this.props.total}</h4>
+                                <h4 style={{ color: "#4A5A67" }}>Total: Bs {this.props.total}</h4>
                             </Col>
                         </Row>
                         <Row className="justify-content-md-center">
                             <Col ls={12}>
-                                <h5>Referencia: #00001</h5>
+                                <h5 style={{ color: "#4A5A67" }}>Referencia: #00001</h5>
                             </Col>
                         </Row>
                         <Row className="justify-content-md-center">
                             <Col ls={12}>
-                                <h5>12/12/2020</h5>
+                                <h5 style={{ color: "#4A5A67" }}>12/12/2020</h5>
                             </Col>
                         </Row>
                         <Row className="justify-content-md-center">
