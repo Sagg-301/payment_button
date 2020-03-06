cb5087bf7624ff5b2fcaa9feadc1dfea283e2876
Sergio <sagg_301@outlook.com>
2020-03-06 15:26:15 -0400

    SAMF call

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -11,37 +11,49 @@ class PaymentForm extends React.Component {
             });
 
         var phone_mask = IMask(document.getElementById('phone'),{
-            mask: '0000-000-0000'
+            mask: '000-0000'
             });
     }
     render() {
         return (
         <Container>
-            <Card style={{ width: '450px' }}>
+            <Card style={{ width: '470px' }}>
                 <Card.Img variant="top" src={logo} />
                 <Card.Body>
                     <Card.Title>Total a pagar: Bs {this.props.total}</Card.Title>
                     <Card.Text>
                         <Form>
                             <Row>
-                                <Col ls={12}>
+                                <Col xs={4} md={4} ls={4}>
+                                    <Form.Group controlId="prefix">
+                                    <Form.Label>Prefijo</Form.Label>
+                                    <Form.Control size="sm" as="select">
+                                        <option>0424</option>
+                                        <option>0414</option>
+                                        <option>0426</option>
+                                        <option>0416</option>
+                                        <option>0412</option>
+                                    </Form.Control>
+                                    </Form.Group>
+                                </Col>
+                                <Col xs={8} md={8} ls={8}>
                                     <Form.Group controlId="phone">
                                     <Form.Label>Teléfono</Form.Label>
-                                    <Form.Control size="sm" type="text" placeholder="0424-111-1111" name="phone" />
+                                    <Form.Control size="sm" type="text" placeholder="***-****" name="phone" />
                                     </Form.Group>
                                 </Col>
                             </Row>
                             <Row>
-                                <Col ls={4}>
+                                <Col xs={4} md={4} ls={4}>
                                     <Form.Group controlId="formBasicEmail">
-                                    <Form.Label>Nacionalidad</Form.Label>
+                                    <Form.Label>Cédula o Rif</Form.Label>
                                     <Form.Control size="sm" as="select">
                                         <option>V</option>
-                                        <option>I</option>
+                                        <option>Rif</option>
                                     </Form.Control>
                                     </Form.Group>
                                 </Col>
-                                <Col ls={8}>
+                                <Col xs={8} md={8} ls={8}>
                                     <Form.Group controlId="formBasicEmail">
                                     <Form.Label>Documento de identidad</Form.Label>
                                     <Form.Control size="sm" type="number" placeholder="Ej 22556854" name="ci" />
@@ -50,7 +62,7 @@ class PaymentForm extends React.Component {
                             </Row>
                             <Row>
                                 <Col ls={12}>
-                                    <Form.Group controlId="formBasicEmail">
+                                    <Form.Group controlId="banco">
                                     <Form.Label>Banco</Form.Label>
                                     <Form.Control size="sm" as="select">
                                         <option>Banco Plaza</option>
@@ -64,7 +76,7 @@ class PaymentForm extends React.Component {
                             </Row>
                             <Row>
                                 <Col ls={12}>
-                                    <Form.Group controlId="formBasicEmail">
+                                    <Form.Group controlId="concepto">
                                     <Form.Label>Descripción</Form.Label>
                                     <Form.Control size="sm" type="text" placeholder="Descripción Aquí" name="description" />
                                     </Form.Group>
@@ -91,7 +103,12 @@ class PaymentForm extends React.Component {
 }
 
 function pay() {
-    window.location.href = './receipt'
+    fetch('http://192.168.137.248:8080/WsSamfGtwRest-1.1/WsSamfmovilGtwRest/resource/GTWC2P/2/U0MyUDtVME15VURzd05ERXlMVFUwT0RVek5UYzdWakl3TVRNeU9EVTVPekF4TURVN05UQXdNQzR3TURzd05ESTJMVGd5TURVeE5qWTdNREV6TkR0SGIyOW5iR1VnUTJoeWIyMWxPMVpsY25OcHc3TnVJRGd3TGpBdU16azROeTR4TWpJN01Ua3lMakUyT0M0NE1pNHhNVFU9/192.168.82.115')
+        .then(res => res.json())
+        .then((data) => {
+          console.log(data)
+        })
+        .catch(console.log)
 }
 
 export default PaymentForm;
--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -6,31 +6,50 @@ class Receipt extends React.Component {
     render() {
         return (
         <Container fluid={true}>
-            <Card style={{ width: '450px' }}>
+            <Card style={{ width: '470px', height: '600px' }}>
                 <Card.Img variant="top" src={check_mark} width="200" height="200" />
                 <Card.Body>
                     <Card.Title> <h3 style={{ color: "#5D9BEB" }}> Pago realizado con éxito </h3></Card.Title>
                     <Card.Text>
-                        <Row className="justify-content-md-center">
-                            <Col ls={12}>
-                                <h4 style={{ color: "#4A5A67" }}>Total: Bs {this.props.total}</h4>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-md-center">
-                            <Col ls={12}>
-                                <h5 style={{ color: "#4A5A67" }}>Referencia: #00001</h5>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-md-center">
-                            <Col ls={12}>
-                                <h5 style={{ color: "#4A5A67" }}>12/12/2020</h5>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-md-center">
-                            <Col ls={12}>
-                                <Button>imprimir</Button>
-                            </Col>
-                        </Row>
+                        <Container fluid={true}>
+                            <Row>
+                                <Col xs={12} md={12} ls={12}>
+                                    <h4 style={{ color: "#4A5A67" }}>Total: Bs {this.props.total}</h4>
+                                </Col>
+                            </Row>
+                            <br/>
+                            <Row>
+                                <Col xs={12} md={12} ls={12}>
+                                    <h5 style={{ color: "#4A5A67"}}>Referencia: #00001</h5>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12} md={12} ls={12}>
+                                    <h5 style={{ color: "#4A5A67" }}>12/12/2020 00:00:00</h5>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12} md={12} ls={12}>
+                                    <h5 style={{ color: "#4A5A67" }}>Teléfono del cliente: 04**-***5695</h5>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12} md={12} ls={12}>
+                                    <h5 style={{ color: "#4A5A67" }}>Banco del cliente: Banco de Venezuela</h5>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12} md={12} ls={12}>
+                                    <h5 style={{ color: "#4A5A67" }}>Descripción: Articulos Varios</h5>
+                                </Col>
+                            </Row>
+                            <br/>
+                            <Row>
+                                <Col xs={12} md={12} ls={12}>
+                                    <Button>imprimir</Button>
+                                </Col>
+                            </Row>
+                        </Container>
                     </Card.Text>
                 </Card.Body>
             </Card>
